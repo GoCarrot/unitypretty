d7c9567d8aab32af7ee2032be29d686440a63705
Pat Wilson <zerostride@gmail.com>
2019-04-23 16:00:16 -0700

    Parse and test the delightful date formats in use by the license check

--- lib/unitypretty/parsers/license.rb
+++ lib/unitypretty/parsers/license.rb
@@ -8,11 +8,26 @@ module UnityPretty
     class License
       include UnityPretty::Parser
 
-      # LICENSE SYSTEM [201921 13:47:26] Next license update check is after 2019-01-31T02:16:03
+      #
+      # Unity engine version and sha
+      #
+      Data = Struct.new(:date, :next_date)
+
       line /^LICENSE SYSTEM \[(.*)\] Next license update check is after (.*)$/ do |date, next_date|
-        puts date
-        puts next_date
-        'bar'
+        date, time = date.split(' ')
+        year = date[0..3]
+        month = date[4..5]
+        day = date[6..-1]
+
+        unless month[0] == '1' && month[1].to_i < 3 && date.length > 6
+          month = date[4]
+          day = date[5..-1]
+        end
+
+        output data: {
+          date: DateTime.strptime("#{year} #{month} #{day} #{time}", '%Y %m %d %H:%M:%S'),
+          next_date: DateTime.parse(next_date)
+        }
       end
     end
   end
--- spec/parsers/license_spec.rb
+++ spec/parsers/license_spec.rb
@@ -1,11 +1,40 @@
 # frozen_string_literal: true
 
 describe UnityPretty::Parsers::License do
-  it 'should contain only Gem::Version objects' do
-    # LICENSE SYSTEM [YYYYdd 13:47:26] Next license update check is after 2019-01-31T02:16:03
-    # LICENSE SYSTEM [201921 13:47:26] Next license update check is after 2019-01-31T02:16:03
-    # foo = UnityPretty::Parsers::License.new
-    # line = 'LICENSE SYSTEM [201921 13:47:26] Next license update check is after 2019-01-31T02:16:03'
-    # expect(foo.match(line)).to equal('bar')
+  describe '#parse' do
+    with_line 'LICENSE SYSTEM [201921 13:47:26] Next license update check is after 2019-01-31T02:16:03' do
+      it { should have_attributes date: DateTime.parse('2019-02-01T13:47:26') }
+      it { should have_attributes next_date: DateTime.parse('2019-01-31T02:16:03') }
+    end
+
+    with_line 'LICENSE SYSTEM [2019422 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-04-22T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
+
+    with_line 'LICENSE SYSTEM [201911 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-01-01T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
+
+    with_line 'LICENSE SYSTEM [2019111 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-11-01T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
+
+    with_line 'LICENSE SYSTEM [201912 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-01-02T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
+
+    with_line 'LICENSE SYSTEM [2019122 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-12-02T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
+
+    with_line 'LICENSE SYSTEM [201913 17:3:2] Next license update check is after 2019-04-23T18:10:19' do
+      it { should have_attributes date: DateTime.parse('2019-01-03T17:03:02') }
+      it { should have_attributes next_date: DateTime.parse('2019-04-23T18:10:19') }
+    end
   end
 end
